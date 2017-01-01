6a83e0ce06a988edcb19e314c09337653b81bd0e
merlox <merloxdixcontrol@gmail.com>
2017-01-01 11:21:03 +0100

    Menu finalizado funcionando perfecto a falta de testear en profundidad. Mejor diseño renovado con iconos elegantes.

--- public/js/menu.js
+++ public/js/menu.js
@@ -26,13 +26,13 @@ window.addEventListener('load', () => {
 	});
 	httpGet('/api/get-logged-state', (state) => {
 		if(state == 'logged'){
-			q('#usuario').innerHTML = 'mi cuenta ▼<div class="triangulo-up"></div>';
+			q('#usuario').innerHTML = '<img src="../../images/user.svg" width="30px">mi cuenta ▼<div class="triangulo-up"></div>';
 			q('#usuario').href = '/micuenta';
 		}else if(state == 'admin'){
-			q('#usuario').innerHTML = 'admin ▼<div class="triangulo-up"></div>';
+			q('#usuario').innerHTML = '<img src="../../images/user.svg" width="30px">admin ▼<div class="triangulo-up"></div>';
 			q('#usuario').href = '/admin';
 		}else{
-			q('#usuario').innerHTML = 'iniciar sesión<div class="triangulo-up"></div>';
+			q('#usuario').innerHTML = '<img src="../../images/user.svg" width="30px">iniciar sesión<div class="triangulo-up"></div>';
 			q('#usuario').href = '/login';
 		}
 	});
@@ -68,7 +68,11 @@ function buscarProducto(keyword){
 	if(keyword.length >= 3){
 		keyword = encodeURIComponent(keyword);
 		httpGet('/api/search/'+keyword+'?limite=8', (results) => {
-			results = JSON.parse(results);
+			try{
+				results = JSON.parse(results);
+			}catch(e){
+				results = null;
+			}
 			q('#buscador-sugerencias').firstChild.innerHTML = '';
 			if(results != null && results != undefined){
 				q('#buscador-sugerencias').style.display = 'block';
@@ -89,7 +93,7 @@ function buscarProducto(keyword){
 					q('#buscador-sugerencias').children[0].innerHTML = htmlProducto;					
 					//Comprobamos el tamaño de la pantalla para hacer responsive los resultados de busqueda
 					//16 Es el espacio de la barra de scroll
-					if((window.outerWidth-16) <= 800 && (window.outerWidth-16) >= 650){
+					if((window.outerWidth-16) <= 800 && ((window.outerWidth-16) >= 650 || (window.outerWidth-16) <= 400)){
 						qAll('.texto-item-busqueda').forEach((e) => {
 							e.innerHTML = e.innerHTML.substring(0, 30) + '...';
 						});
@@ -109,23 +113,58 @@ function buscarProducto(keyword){
 let flechaActiva = false;
 function cambiarColorFlechaDepartamentos(){
 	if(!flechaActiva){
-		q('#flecha-departamentos').style.borderBottomColor = 'lightblue';
+		if(q('#triangulo-up-sesion')){
+			if((window.outerWidth-16) <= 1000 && (window.outerWidth-16) >= 650) q('#triangulo-up-sesion').style.borderBottomColor = '#74b1ff';
+			else q('#triangulo-up-sesion').style.borderBottomColor = '#91c1ff';
+		}
+		if(q('#flecha-departamentos')) q('#flecha-departamentos').style.borderBottomColor = '#91c1ff';
 		flechaActiva = true;
 	}else{
-		q('#flecha-departamentos').style.borderBottomColor = 'white';
+		if(q('#triangulo-up-sesion')) q('#triangulo-up-sesion').style.borderBottomColor = 'white';
+		if(q('#flecha-departamentos')) q('#flecha-departamentos').style.borderBottomColor = 'white';
 		flechaActiva = false;
 	}
 };
+function ocultarCestaBuscadorHoverDropdown(ocultarCesta){
+	if(q('#productos-cesta') && (ocultarCesta == undefined || ocultarCesta == null)){
+		q('#productos-cesta').style.display = 'none';
+		q('span.triangulo-up').style.display = 'none';
+	}
+	if(q('#buscador-sugerencias')){
+		q('#buscador').style.borderRadius = '5px 0 0 5px';
+		q('#buscador-sugerencias').style.display = 'none';
+	}
+}
 
-//Ocultar o mostrar el menu de departamentos
-q('#departamentos').addEventListener('mouseenter', () => {
-	ocultarMostrarDepartamentos();
+/*
+
+CESTA (LO IMPORTANTE ESTÁ EN CESTA.JS)
+
+*/
+//Para ocultar el buscador al hover cesta
+q('a#cesta').addEventListener('mouseenter', () => {
+	ocultarCestaBuscadorHoverDropdown(false);
 });
 
+/*
+
+DEPARTAMENTOS
+
+*/
+//Ocultar o mostrar el menu de departamentos
+q('#departamentos').addEventListener('mouseenter', () => {
+	ocultarCestaBuscadorHoverDropdown();
+	ocultarMostrarDepartamentos();
+});
 q('#departamentos').addEventListener('mouseleave', () => {
 	ocultarMostrarDepartamentos();
 });
 
+/*
+
+BUSCADOR
+
+*/
 //Para buscar sugerencias a cada toque
 q('#buscador').addEventListener('keyup', () => {
 	buscarProducto(q('#buscador').value);
@@ -133,34 +172,37 @@ q('#buscador').addEventListener('keyup', () => {
 //Al hacer focus al input
 q('#buscador').addEventListener('focus', () => {
 	buscarProducto(q('#buscador').value);
+	ocultarCestaBuscadorHoverDropdown();
 });
-
+q('#buscador').addEventListener('click', (e) => {
+	buscarProducto(q('#buscador').value);
+	ocultarCestaBuscadorHoverDropdown();
+	e.stopPropagation();
+});
 q('html').addEventListener('click', () => {
 	q('#buscador').style.borderRadius = '5px 0 0 5px';
 	q('#buscador-sugerencias').style.display = 'none';
 });
-
 //Para ir a la página de busqueda
 q('#icono-busqueda').addEventListener('click', () => {
 	//Si la busqueda no esta vacía, redirigir a resultados de busqueda en caso de hacer click a buscar
 	if(q('#buscador').value != null && q('#buscador').value != "" && q('#buscador').value != undefined){
-		window.location = '//192.168.1.100:8000/search/?searched='+encodeURI(q('#buscador').value);
+		window.location.href = '/search/?searched='+encodeURI(q('#buscador').value);
 	}
 });
 
+/*
+
+SESIÓN
+
+*/
 //Para mostrar el desplegable de opciones de sesion al hover sesion
-q('#usuario').addEventListener('mouseenter', () => {
+q('#contenedor-login-dropdown').addEventListener('mouseenter', () => {
+	ocultarCestaBuscadorHoverDropdown();
+	q('#triangulo-up-sesion').style.display = 'block';
 	q('#menu-sesion').style.display = 'block';
 });
-
-//No ocultar si se hace click sobre el contenido cesta
-q('#menu-sesion').addEventListener('click', (e) => {
-	e.stopPropagation();
-});
-//Ocultar la cesta al hacer click fuera
-q('body').addEventListener('click', (e) => {
-	if(e.target != q('#usuario')){
-		q('#menu-sesion').style.display = 'none';
-		q('.triangulo-up').style.display = 'none';
-	}
+q('#contenedor-login-dropdown').addEventListener('mouseleave', () => {
+	q('#triangulo-up-sesion').style.display = 'none';
+	q('#menu-sesion').style.display = 'none';
 });

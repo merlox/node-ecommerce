399ce0fc2db0e38297cfefc683db91b68a143077
merlox <merloxdixcontrol@gmail.com>
2017-03-05 14:17:46 +0100

    Quitado secrets de github

--- server/secrets/secrets.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	'emailUsername': 'postmaster@mg.kamisetaskustom.com',
-	'emailPassword': 'a06866b9803195d1338cc42b40e4adf8',
-	'stripeSecret': 'sk_live_AVcZnt1K6XSf4etb13lJhI1v'
-};

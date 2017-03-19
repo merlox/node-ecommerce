097b6c96bceaad7dc45f3b9ab0d415884700aa87
merlox <merloxdixcontrol@gmail.com>
2017-03-19 09:59:41 +0000

    Cambiada la key  publica de stripe

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,6 +1,6 @@
 'use strict';
 
-Stripe.setPublishableKey('pk_test_UVgGQ5OoCEARrRvds8bKYV93');
+Stripe.setPublishableKey('pk_live_9bAIPTRft8m2tWvGJgLSS4tO');
 
 window.addEventListener('load', () => {
 	renderCesta();
